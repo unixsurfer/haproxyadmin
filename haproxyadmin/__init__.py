96f09d729faf337cd0b5a71d267b810fc682fd13
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-09-13 23:58:30 +0200

    RELEASE 0.1.5 version

--- haproxyadmin/__init__.py
+++ haproxyadmin/__init__.py
@@ -5,5 +5,5 @@
 __title__ = 'haproxyadmin'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __copyright__ = 'Copyright 2015 Pavlos Parissis'
