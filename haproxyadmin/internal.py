unixsurfer/haproxyadmin

commit dcc5173e31deac9fd77ae6694605f42f343ea8c7

simplify the way we send commands to socket
We used to have one function(send_command) which was sending a command
to haproxy socket and was returning what socket returned to us.
Later we added a wrapper function(run_command) to filter the output, either
to return 1st line or the whole output. This layout adds extra code without any
real benefit. We just add full_output parameter to send_command to support the
same behavior and rename send_command to command.

haproxyadmin/internal.py
@@ -47,13 +47,18 @@ class _HAProxyProcess(object):
         # process number associated with this object
         self.process_nb = self.metric('Process_num')
 
-    def send_command(self, command):
+    def command(self, command, full_output=False):
         """Send a command to HAProxy over UNIX stats socket.
+
+        Newline character returned from haproxy is stripped off.
 
         :param command: A valid command to execute
         :type command: string
-        :return: Output. Newline character is stripped off.
-        :rtype: list
+        :param full_output: (optional) Return all output, by default
+          returns only the 1st line of the output
+        :type full_output: ``bool``
+        :return: 1st line of the output or the whole output as a list
+        :rtype: ``string`` or ``list`` if full_output is True
         """
         data = []
         for attempt in range(1, self.retry + 1):
@@ -104,32 +109,17 @@ class _HAProxyProcess(object):
             finally:
                 unix_socket.close()
 
-        return data
-
-    def run_command(self, command, full_output=False):
-        """Run a command to HAProxy process.
-
-        :param command: A valid command to execute
-        :type command: string
-        :param full_output: (optional) Return all output, by default
-          returns only the 1st line of the output
-        :type full_output: bool
-        :return: 1st line of the output or the whole output as a list
-        :rtype: string or list if full_output is True
-        """
-        output = self.send_command(command)
-
         if full_output:
-            return output
+            return data
         else:
-            return output[0]
+            return data[0]
 
     def proc_info(self):
         """Return a dictionary containing information about HAProxy daemon.
 
         :rtype: dictionary, see utils.info2dict() for details
         """
-        raw_info = self.send_command('show info')
+        raw_info = self.command('show info')
 
         return info2dict(raw_info)
 
@@ -154,9 +144,9 @@ class _HAProxyProcess(object):
         :type sid: ``integer``
         :rtype: dict, see ``utils.stat2dict`` for details on the structure
         """
-        csv_data = self.send_command('show stat {} {} {}'.format(iid,
-                                                                 obj_type,
-                                                                 sid))
+        csv_data = self.command('show stat {} {} {}'.format(iid,
+                                                            obj_type,
+                                                            sid))
         self.hap_stats = stat2dict(csv_data)
         return self.hap_stats
 
@@ -343,7 +333,7 @@ class _Frontend(object):
         :return: 1st line of the output.
         :rtype: ``string``
         """
-        return self.hap_process.run_command(cmd)
+        return self.hap_process.command(cmd)
 
 
 class _Backend(object):
@@ -420,7 +410,7 @@ class _Backend(object):
         return converter(getattr(data, name))
 
     def command(self, cmd):
-        return self.hap_process.run_command(cmd)
+        return self.hap_process.command(cmd)
 
     def servers(self, name=None):
         """Return a list of _Server objects for each server of the backend.
@@ -521,4 +511,4 @@ class _Server(object):
         return dict(zip(keys, values))
 
     def command(self, cmd):
-        return self.backend.hap_process.run_command(cmd)
+        return self.backend.hap_process.command(cmd)

haproxyadmin/haproxy.py
@@ -158,7 +158,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -194,7 +194,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -224,7 +224,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -254,7 +254,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -277,7 +277,7 @@ class HAProxy(object):
         else:
             cmd = "clear counters"
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -353,7 +353,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -398,7 +398,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        results = cmd_across_all_procs(self._hap_processes, 'command',
                                        cmd)
 
         return check_command(results)
@@ -424,8 +424,8 @@ class HAProxy(object):
         else:
             cmd = "show errors"
 
-        return cmd_across_all_procs(self._hap_processes, 'run_command',
-                                    cmd, True)
+        return cmd_across_all_procs(self._hap_processes, 'command',
+                                    cmd, full_output=True)
 
     def frontends(self, name=None):
         """Build a list of :class:`Frontend <haproxyadmin.frontend.Frontend>`
@@ -503,8 +503,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        get_results = cmd_across_all_procs(self._hap_processes, 'run_command',
-                                           cmd)
+        get_results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
         get_info_proc1 = get_results[0][1]
         if not check_output(get_info_proc1):
             raise ValueError(get_info_proc1)
@@ -540,7 +539,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        get_results = cmd_across_all_procs(self._hap_processes, 'run_command',
+        get_results = cmd_across_all_procs(self._hap_processes, 'command',
                                            cmd)
         get_info_proc1 = get_results[0][1]
         if not check_output(get_info_proc1):
@@ -755,8 +754,7 @@ class HAProxy(object):
         else:
             raise ValueError("Invalid input")
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command',
-                                       cmd)
+        results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
 
         return check_command(results)
 
@@ -780,7 +778,7 @@ class HAProxy(object):
             raise ValueError("Expected integer and got {}".format(type(value)))
         cmd = "set maxconn global {}".format(value)
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command', cmd)
+        results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
 
         return check_command(results)
 
@@ -798,7 +796,7 @@ class HAProxy(object):
             raise ValueError("Expected integer and got {}".format(type(value)))
         cmd = "set rate-limit connections global {}".format(value)
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command', cmd)
+        results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
 
         return check_command(results)
 
@@ -816,7 +814,7 @@ class HAProxy(object):
             raise ValueError("Expected integer and got {}".format(type(value)))
         cmd = "set rate-limit sessions global {}".format(value)
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command', cmd)
+        results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
 
         return check_command(results)
 
@@ -834,9 +832,10 @@ class HAProxy(object):
             raise ValueError("Expected integer and got {}".format(type(value)))
         cmd = "set rate-limit ssl-sessions global {}".format(value)
 
-        results = cmd_across_all_procs(self._hap_processes, 'run_command', cmd)
+        results = cmd_across_all_procs(self._hap_processes, 'command', cmd)
 
         return check_command(results)
+
 
     @should_die
     def show_acl(self, aclid=None):

commit ec141a0a956d68778f93f9647950d71afbc63adb

fix a regression introduced with dcc5173e31deac

haproxyadmin/internal.py
@@ -119,7 +119,7 @@ class _HAProxyProcess(object):
 
         :rtype: dictionary, see utils.info2dict() for details
         """
-        raw_info = self.command('show info')
+        raw_info = self.command('show info', full_output=True)
 
         return info2dict(raw_info)
 
@@ -144,9 +144,8 @@ class _HAProxyProcess(object):
         :type sid: ``integer``
         :rtype: dict, see ``utils.stat2dict`` for details on the structure
         """
-        csv_data = self.command('show stat {} {} {}'.format(iid,
-                                                            obj_type,
-                                                            sid))
+        csv_data = self.command('show stat {} {} {}'.format(iid, obj_type, sid),
+                                full_output=True)
         self.hap_stats = stat2dict(csv_data)
         return self.hap_stats
 