f130fe243cb5a991e01a87cae5cbc2ed4aeab9ed
Christian Rovner <crovner@gmail.com>
2021-03-16 11:16:58 +0100

    RELEASE 0.2.4 version

--- haproxyadmin/__init__.py
+++ haproxyadmin/__init__.py
@@ -10,7 +10,7 @@ A python library to interact with HAProxy over UNIX socket
 __title__ = 'haproxyadmin'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 __copyright__ = 'Copyright 2015-2019 Pavlos Parissis'
 
 from haproxyadmin.haproxy import HAPROXY_METRICS
